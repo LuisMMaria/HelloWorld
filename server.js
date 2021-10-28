3c34d6809c85d0395c372bd5b6d5c9f5edabdc1c
unknown <luismaria_95@hotmail.com>
2021-10-27 21:19:45 -0300

    Segundo commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 const app = express();
 
 app.get('/saludo', (req, resp) => {
-    resp.json("Hello World")
+    resp.send("Hello World")
 })
 
 app.listen(8080, () => {
